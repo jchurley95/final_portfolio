0aaf18e72196d42871d37edcbd92e89b3c0e0a0f
jchurley95 <jchurley95@gmail.com>
2017-09-22 03:41:20 -0400

    Added create show update destroy and project_params to projects_controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -6,5 +6,33 @@ class Api::ProjectsController < ApplicationController
         render json: @projects
     end
 
+    def create
+        @project = Project.create!(project)_params)
+        redirect_to project_path(@project)
+    end
+
+    def show
+        @project = Project.find(params[:id])
+        render json: @project
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        @project.update!(project_params)
+        redirect_to artist_path(@project)
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+        redirect_to projects_path
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :description, :pictureURL, :deployedURL, :githubURL, :backgroundColor)
+    end
+
 
 end
